cc32639e05d7ce6612d1a925d8a176f4fd317e43
Eric Ye <computereric@live.ca>
2012-05-18 11:06:49 -0400

    More functions to filter non-IoOs--add support for pans.

--- IoOUtil.py
+++ IoOUtil.py
@@ -6,12 +6,46 @@ def listImgs(AP, GTS):
     In other words, the images that are *not* images of opportunity.'''
 
     from extract import mapping, extract
+    #Select the targets that produce image numbers
     relKeys = [key for key in mapping if key[-3:] == 'IMG']
     imgs = []
     for key in relKeys:
         dscnNum = extract(key, AP, GTS)
-        if dscnNum:
+        if dscnNum:#if the image is there
             imgs.append(str(int(dscnNum)).zfill(4))
+
+    ##TODO: Add support for pans
     return imgs
 
 #TODO: finish this.
+def selectImgs(AP, GTS):
+    '''A list of Images of Opportunity available for a given AP and GTS.'''
+    from os import listdir
+    from read import makePath
+    import re
+
+    #Create a list 'allPics' that stores the names of all the pictures in 'raw'
+        #for the given AP and GTS.
+    try:
+        allPics = [picture for picture in
+                   listdir(makePath(AP, GTS, pics = True))
+                   if picture[-4:].upper() == ".JPG"]
+        
+    except: #if you can't find anything
+        print "No pictures found for AP{0}-{1}".format(AP, GTS)
+        return
+
+    #for example, 'DSC_' or 'IMG_'--we only want the numbers
+    head = allPics[0][:-8] 
+
+    #strip away the head and .jpg.
+    pics = [pic[-8:-4] for pic in allPics]
+    #filter the images that are accounted for by the FDS
+    FDSImgs = listImgs(AP, GTS)
+    pics = [pic for pic in pics if pic not in FDSImgs]
+
+    #put the header and .jpg back on:
+    pics = ['{0}{1}.JPG'.format(head, pic) for pic in pics]
+
+    print pics
+    
--- read.py
+++ read.py
@@ -1,11 +1,25 @@
 
-def makePath(AP, GTS, t = None): #If t is not supplied, GTS
+def makePath(AP, GTS, t = None, pics = False): #If t is not supplied, GTS
                                  #is the GTS. Else, it is the G-value.
     '''Takes an AP and GTS number and returns
-    the Windows Path to the .dat file as a string.'''
+    the Windows Path to the .dat file as a string.
+
+    If t is supplied, GTS is taken as the G-value and t the T-value.
+    
+    If pics is True, return a path to the images directory instead.
+    For IoO people.
+
+    If both t and pics are specified, which should never happen, pics has precedence.'''
 
     base = 'AHPDATA\\AP{0}\\'.format(AP)
+    G = GTS/100
+    T = (GTS-G*100) / 10
+    S = GTS%10
     
+    if pics:
+        return '{bases}FIT{0}\\T{1}\\SS{2}\\Images\\Raw'.format(G, T, S, bases = base)
+
+
     if t: #create directory for the traverse rather than for the specific
             #sample station
         return '{bases}FIT{0}\\T{1}'.format(GTS, t, bases = base)
@@ -19,21 +33,18 @@ def makePath(AP, GTS, t = None): #If t is not supplied, GTS
     #AP13 GTS 622 should be:
     #AHPDATA\AP13\FIT6\T2\Data\13-622.dat
     else:
-        G = GTS/100
-        T = (GTS-G*100) / 10
         return r'{bases}FIT{1}\T{2}\Data\{3}-{4}.dat'.format(\
             AP, G, T, AP, GTS, bases = base)
 
 def openFile(AP, GTS):
     '''Open the .dat file and split it.'''
-    #First, let's check it exists.
-    from os.path import exists
-    if not exists(makePath(AP, GTS)):
-        print "AP{0}-{1} does not exist.".format(AP, GTS)
-        return    
     loc = makePath(AP, GTS)
-    with open(loc, 'r') as fil:
-        f = fil.read().split()
+    try:
+        with open(loc, 'r') as fil:
+            f = fil.read().split() #read the file into the variable f.
+    except IOError: #If the file is not there
+        print "AP{0}-{1} does not exist.".format(AP, GTS)
+        return
     return f
 
 def existGTS(AP, GTS):
