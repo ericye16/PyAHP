cdb0b02578b41447e9268fff02b58ec0af1ef500
Eric Ye <computereric@live.ca>
2012-05-24 00:29:43 -0400

    Reduced complexity. Undid last commit.

--- PyAHP_main.py
+++ PyAHP_main.py
@@ -10,26 +10,29 @@ def main():
     while True:
         wanted = raw_input('What field?')
         if wanted not in mapping:
-            print 'Please look at the README to see what you can look up.'
+            print 'Please look at the README to see what fields are available.'
         else:
             break
 
-    while True:
-        wantedGTS = raw_input('Specific GTS? (y/n): ')
-        if wantedGTS.lower() == 'y':
-            specificGTS = True
-            wantedGTS = raw_input('Which one? ')
-            break
-        elif wantedGTS.lower() == 'n':
-            specificGTS = False
-            break
+# Too much complexity.
+
+##    while True:
+##        wantedGTS = raw_input('Specific GTS? (y/n): ')
+##        if wantedGTS.lower() == 'y':
+##            specificGTS = True
+##            wantedGTS = raw_input('Which one? ')
+##            break
+##        elif wantedGTS.lower() == 'n':
+##            specificGTS = False
+##            break
             
     
     print 'Collecting data...'
-    if specificGTS:
-        data = forAllAPs(wanted, wantedGTS)
-    else:
-        data = forAllAPsGTS(wanted)
+##    if specificGTS:
+##        data = forAllAPs(wanted, wantedGTS)
+##    else:
+##        data = forAllAPsGTS(wanted)
+    data = forAllAPsGTS(wanted)
     print 'Data collected. Writing to {0}.csv'.format(wanted)
        
     writeToCSV(data, wanted)
