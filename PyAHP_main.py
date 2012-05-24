919d4b9d8a9720f34accf048c2f9fea34e0be758
Eric Ye <computereric@live.ca>
2012-05-23 23:02:29 -0400

    Added support to search one specific GTS.
    Not sure if this is adding too much complexity to the program.

--- PyAHP_main.py
+++ PyAHP_main.py
@@ -4,18 +4,32 @@ def main():
     '''Main function for PyAHP.'''
 
     from extract import mapping
-    from util import forAllAPsGTS
+    from util import forAllAPsGTS, forAllAPs
     from wutils import writeToCSV
     
     while True:
-        wanted = raw_input('What would you like to look for?')
+        wanted = raw_input('What field?')
         if wanted not in mapping:
             print 'Please look at the README to see what you can look up.'
         else:
             break
+
+    while True:
+        wantedGTS = raw_input('Specific GTS? (y/n): ')
+        if wantedGTS.lower() == 'y':
+            specificGTS = True
+            wantedGTS = raw_input('Which one? ')
+            break
+        elif wantedGTS.lower() == 'n':
+            specificGTS = False
+            break
+            
     
     print 'Collecting data...'
-    data = forAllAPsGTS(wanted)
+    if specificGTS:
+        data = forAllAPs(wanted, wantedGTS)
+    else:
+        data = forAllAPsGTS(wanted)
     print 'Data collected. Writing to {0}.csv'.format(wanted)
        
     writeToCSV(data, wanted)
